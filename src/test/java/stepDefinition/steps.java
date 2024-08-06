96cdec7b1cc56fe167d975cb9eb71ef7a903132d
victor <vit10k89.beiu@gmail.com>
2024-08-06 15:04:28 +0300

    First commit

---
+++ src/test/java/stepDefinition/steps.java
@@ -0,0 +1,31 @@
+package stepDefinition;
+
+import io.cucumber.java.en.*;
+
+public class steps {
+
+    @Given("the user is on the nopCommerce login page")
+    public void the_user_is_on_the_nop_commerce_login_page() {
+
+    }
+
+    @When("the user enters valid credentials \\(username: {string}, password: {string})")
+    public void the_user_enters_valid_credentials_username_password(String string, String string2) {
+
+    }
+
+    @When("the user clicks on the Login button")
+    public void the_user_clicks_on_the_login_button() {
+
+    }
+
+    @Then("the user should be redirected to the My Account page")
+    public void the_user_should_be_redirected_to_the_my_account_page() {
+
+    }
+
+    @Then("the user should see a welcome message")
+    public void the_user_should_see_a_welcome_message() {
+
+    }
+}
